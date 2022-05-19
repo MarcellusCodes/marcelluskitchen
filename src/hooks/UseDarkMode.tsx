41ff46ea76d0701c6a713e06a551ba4f154d22cd
MarcellusCodes <MarcellusCodes@gmail.com>
2022-05-19 11:10:11 +0200

    Added Darkmode

---
+++ src/hooks/UseDarkMode.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export default function UseDarkMode() {
+  if (typeof window !== "undefined") {
+    const StorageCheck = localStorage.getItem("Theme") || "dark";
+
+    const [Theme, SetTheme] = useState(StorageCheck);
+    const ColorTheme = Theme === "light" ? "dark" : "light";
+
+    useEffect(() => {
+      localStorage.setItem("Theme", Theme);
+      const root = window.document.documentElement;
+      root.classList.remove(ColorTheme);
+      root.classList.add(Theme);
+    }, [Theme, ColorTheme]);
+    return [ColorTheme, SetTheme];
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         primary: "#1F2028",
         secondary: "#2E3039",
+        tertiary: "#a9adc1",
       },
       fontFamily: {
         primary: ["Oregano", "cursive"],
